002663df39da58cbcbd08e096e37460df4ce583c
Ashik Ahammad <82080802+Ashik-Ahammad@users.noreply.github.com>
2026-07-11 12:34:03 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   /* config options here */
   serverExternalPackages: ['@better-auth/kysely-adapter', 'kysely'],
+  output: "standalone",
   reactCompiler: true,
    images: {
     remotePatterns: [
