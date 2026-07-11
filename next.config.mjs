de132e9aa7437f194b3c074c68a3ae804e0843d6
Ashik Ahammad <82080802+Ashik-Ahammad@users.noreply.github.com>
2026-07-11 14:54:32 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,67 +1,29 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-
-  /* config options here */
-
   serverExternalPackages: ['@better-auth/kysely-adapter', 'kysely'],
-
   output: "standalone",
-
   reactCompiler: true,
-
-  
-
-  env: {
-
-    NEXT_PUBLIC_CLIENT_URL: process.env.NEXT_PUBLIC_CLIENT_URL || "http://localhost:3000",
-
-    NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:5000",
-
-  },
-
- 
-
   eslint: {
-
     ignoreDuringBuilds: true,
-
   },
-
   typescript: {
-
     ignoreBuildErrors: true,
-
   },
-
-
+  staticPageGenerationTimeout: 1000,
   images: {
-
+    unoptimized: true,
     remotePatterns: [
-
       {
-
         protocol: 'https',
-
         hostname: '**',
-
       },
-
     ],
-
   },
-
   experimental: {
-
     serverActions: {
-
       bodySizeLimit: '5mb',
-
     },
-
   },
-
 };
-
 
 export default nextConfig;
